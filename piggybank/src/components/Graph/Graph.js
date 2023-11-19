b4b32322a36f31578b01d3d606719baac3aa8fa9
Rigpea <rigpea@bu.edu>
2023-11-19 07:03:36 -0500

    UI Inputs

--- piggybank/src/components/Graph/Graph.js
+++ piggybank/src/components/Graph/Graph.js
@@ -1,52 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ReferenceLine } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ReferenceLine, Label } from 'recharts';
+import "./Graph.css";
 
 const Graph = () => {
   const [data, setData] = useState([]);
+  const [initialBalance, setInitialBalance] = useState(0);
   const [monthlyAllowance, setMonthlyAllowance] = useState(0);
-  const [itemPrice, setItemPrice] = useState(null);
+  const [itemPrice, setItemPrice] = useState(0);
 
   useEffect(() => {
     // Fetch total balance
     fetch('http://localhost:8080/api/get-total-balance')
       .then(response => response.json())
-      .then(balanceData => {
-        const initialBalance = balanceData.total_balance || 0;
-
-        // Fetch allowance
-        fetch('http://localhost:8080/api/get-allowance')
-          .then(response => response.json())
-          .then(allowanceData => {
-            if (allowanceData.allowance) {
-              setMonthlyAllowance(allowanceData.allowance);
-              const projections = Array.from({ length: 12 }).map((_, index) => ({
-                month: index + 1,
-                balance: initialBalance + allowanceData.allowance * index
-              }));
-              setData(projections);
-            }
-          })
-          .catch(error => console.error('Error fetching allowance:', error));
+      .then(data => {
+        const balance = data.total_balance || 0;
+        setInitialBalance(parseFloat(balance));
       })
       .catch(error => console.error('Error fetching total balance:', error));
 
-    // Fetch item price
-    fetch('http://localhost:8080/api/get-item')
+    // Fetch allowance
+    fetch('http://localhost:8080/api/get-allowance')
       .then(response => response.json())
-      .then(itemData => {
-        if (itemData.price) {
-          setItemPrice(itemData.price);
-        }
+      .then(data => {
+        const allowance = data.allowance || 0;
+        setMonthlyAllowance(parseFloat(allowance));
+      })
+      .catch(error => console.error('Error fetching allowance:', error));
+
+    // Fetch item price
+    fetch('http://localhost:8080/api/get-price')
+      .then(response => response.json())
+      .then(data => {
+        const price = data.price || 0;
+        setItemPrice(parseFloat(price));
       })
       .catch(error => console.error('Error fetching item price:', error));
   }, []);
 
+  useEffect(() => {
+    if (initialBalance && monthlyAllowance) {
+      const projections = Array.from({ length: 12 }).map((_, index) => ({
+        month: index + 1,
+        balance: initialBalance + monthlyAllowance * index
+      }));
+      setData(projections);
+    }
+  }, [initialBalance, monthlyAllowance]);
+
   return (
-    <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 30, bottom: 30, left: 30 }}>
       <Line type="monotone" dataKey="balance" stroke="#8884d8" />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-      <XAxis dataKey="month" />
-      <YAxis />
+      <XAxis dataKey="month">
+        <Label value="Months" offset={-10} position="insideBottom" />
+      </XAxis>
+      <YAxis label={{ value: 'Balance ($)', angle: -90, position: 'insideLeft' }} />
       <Tooltip />
       {itemPrice && (
         <ReferenceLine y={itemPrice} label={`Item Price: $${itemPrice}`} stroke="red" strokeDasharray="3 3" />
@@ -54,5 +62,4 @@ const Graph = () => {
     </LineChart>
   );
 };
-
 export default Graph;
--- piggybank/src/pages/ChildPage/ChildPage.js
+++ piggybank/src/pages/ChildPage/ChildPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ItemBox from '../../components/ItemBox/ItemBox';  // Ensure correct path
 import Balance from '../../components/Balance/Balance';  // Ensure correct path
 import ChoreList from '../../components/ChoreList/ChoreList';  // Ensure correct path
+import Graph from '../../components/Graph/Graph'
 
 const ChildPage = () => {
     return (
@@ -9,6 +10,7 @@ const ChildPage = () => {
             <ItemBox />
             <Balance />
             <ChoreList />
+            <Graph/>
         </div>
     );
 };
